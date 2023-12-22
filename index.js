4f3e0418869360fac860fcb3fac4e4775446f75d
MrLuisKiller <luis_bol_1998@hotmail.com>
2023-12-21 20:37:38 -0700

    Seguridad y Router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const router = express.Router()
 const cors = require('cors')
 const port = process.env.PORT
 
@@ -20,32 +21,58 @@ app.get('/', (req, res) => {
     res.send('API de Platillos UCamp v1.0')
 })
 
-app.get('/platillos', (req, res) => {
-    res.json({
-        message: 'Platillos displonibles',
-        data: platillos
+app.use('/platillos', router)
+
+router.get('/', (req, res) => {
+    let status = 404
+    let message = 'Ningun platillo disponible'
+    let data = null
+    if (platillos.length > 0) {
+        status = 200
+        message = 'Platillos disponibles'
+        data = platillos
+    }
+    res.status(status).json({
+        message,
+        data
     })
 })
 
-app.post('/platillos', (req, res) => {
-    platillos.push(req.body)
-    res.json({
-        message: 'Se agrego el platillo',
+router.post('/', (req, res) => {
+    let status = 404
+    let message = 'ID existente'
+    const { id } = req.body
+    if (!platillos.find(platillo => platillo.id == id)) {
+        status = 200
+        message = 'Se agrego el platillo'
+        platillos.push(req.body)
+    }
+    res.status(status).json({
+        message,
         data: req.body
     })
 })
 
-app.put('/platillos', (req, res) => {
+router.put('/', (req, res) => {
     const id = parseInt(req.query.id)
-    let status = 404
-    let message = 'Platillo inexistente'
-    if (platillos.find(platillo => platillo.id == id)) {
+    let status = 400
+    let message = 'ID necesaria'
+    if (id) {
+        message = 'nombre o precio necesarios'
         const { nombre, precio } = req.body
-        const index = platillos.findIndex(platillo => platillo.id == id)
-        platillos[index].nombre = nombre
-        platillos[index].precio = precio
-        status = 200
-        message = 'Se actualizo el platillo'
+        if (nombre || precio) {
+            status = 404
+            message = 'Platillo inexistente'
+            if (platillos.find(platillo => platillo.id == id)) {
+                const index = platillos.findIndex(platillo => platillo.id == id)
+                if (nombre)
+                    platillos[index].nombre = nombre
+                if (precio)
+                    platillos[index].precio = precio
+                status = 200
+                message = 'Se actualizo el platillo'
+            }
+        }
     }
     res.status(status).json({
         message,
@@ -53,22 +80,24 @@ app.put('/platillos', (req, res) => {
     })
 })
 
-app.delete('/platillos', (req, res) => {
+router.delete('/', (req, res) => {
     const id = parseInt(req.query.id)
-    let status = 404
-    let message = 'Platillo inexistente'
-    if (platillos.find(platillo => platillo.id == id)) {
-        const index = platillos.findIndex(platillo => platillo.id == id)
-        platillos.splice(index, 1)
-        status = 200
-        message = 'Se elimino el platillo'
+    let status = 400
+    let message = 'ID necesaria'
+    if (id) {
+        status = 404
+        message = 'Platillo inexistente'
+        if (platillos.find(platillo => platillo.id == id)) {
+            const index = platillos.findIndex(platillo => platillo.id == id)
+            platillos.splice(index, 1)
+            status = 200
+            message = 'Se elimino el platillo'
+        }
     }
     res.status(status).json({
         message,
         data: null
     })
 })
-
-
 
 app.listen(port, () => console.log(`Servidor escuchando en http://localhost:${port}`))
